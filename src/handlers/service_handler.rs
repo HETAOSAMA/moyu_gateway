d664752464e35714f3e6234b2b84b9e80065221f
bv47 <1131573373@qq.com>
2024-01-02 15:46:13 +0800

    稍微修改了一下，但是还是没头绪

--- src/handlers/service_handler.rs
+++ src/handlers/service_handler.rs
@@ -1,13 +1,17 @@
 use actix_web::{HttpRequest, HttpResponse, Responder, web};
+use serde_json::from_str;
+use crate::domain::table::sys_services;
+use crate::domain::table::sys_services::SysServices;
 use crate::service::redis_service::get;
 
 // 处理具体路径的逻辑
 async fn handle_specific_path(service_info: String, path: &str) -> Result<HttpResponse, actix_web::Error>{
     // 解析服务信息
-    let parts: Vec<&str> = service_info.split('/').collect();
-    let domain = parts[0];
-    let port: u16 = parts[1].parse().expect("Failed to parse port");
-    let protocol = parts[2];
+    println!("service_info:{}",service_info);
+    let sys_services:SysServices = from_str(&service_info).unwrap();
+    let domain = sys_services.url.unwrap();
+    let port: u16 = sys_services.port.unwrap().try_into().unwrap();
+    let protocol = sys_services.protocol.unwrap();
 
     // 构建目标URL
     let target_url = format!("{}://{}:{}/{}", protocol, domain, port, path);
@@ -27,26 +31,31 @@ pub async fn handle_request(
     // 尝试匹配具体路径
     let specific_key = format!("{}/{}", service, path);
     let json_string = get(specific_key.to_string()).await.await;
-    println!("NEI--------RONG: {:?}", json_string);
 
     match json_string {
-        Ok(service_info) => {
-            // 处理具体路径
-            handle_specific_path(service_info, &path).await
+        Ok(ref service_info) => {
+            if json_string.clone().unwrap().is_empty() {
+                let wildcard_key = format!("{}/{}", service, "*");
+                let wildcard_result: Result<String, String> = get(wildcard_key.to_string()).await.await.map_err(|_| "Service not found".to_string());
+                // 如果成功匹配通配符路径
+                if let Ok(service_info) = wildcard_result {
+                    // 处理通配符路径
+                    if wildcard_result.unwrap().is_empty() {
+                        return Ok(HttpResponse::NotFound().body("Service not found"));
+                    }else {
+                        return handle_specific_path(service_info, &path).await;
+                    }
+                } else {
+                    // 如果无法匹配具体路径和通配符路径，则返回HTTP 404 Not Found响应
+                    Ok(HttpResponse::NotFound().body("Service not found"))
+                }
+            }else {
+                // 处理具体路径
+                handle_specific_path(service_info.clone(), &path).await
+            }
         }
         Err(_) => {
-            // 尝试匹配通配符路径
-            let wildcard_key = format!("{}/{}", service, "*");
-            let wildcard_result: Result<String, String> = get(wildcard_key.to_string()).await.await.map_err(|_| "Service not found".to_string());
-
-            // 如果成功匹配通配符路径
-            if let Ok(service_info) = wildcard_result {
-                // 处理通配符路径
-                handle_specific_path(service_info, &path).await
-            } else {
-                // 如果无法匹配具体路径和通配符路径，则返回HTTP 404 Not Found响应
-                Ok(HttpResponse::NotFound().body("Service not found"))
-            }
+            return Ok(HttpResponse::NotFound().body("Service not found"));
         }
     }
 }
